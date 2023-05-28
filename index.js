14bf719af83bb88a501b534147192c2c6e625767
Andrew <a.oppman@gmail.com>
2023-05-28 01:13:25 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+/*function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then ((json) => console.log(renderBooks(json)))
+}*/
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const books = fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then((json) => renderBooks(json))
+ return books
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
